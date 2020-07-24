d9cdd4713ba2421494ae5a36bb7ca3241ba094bf
MMendes24 <mmbusiness1@protonmail.com>
2020-07-24 10:45:12 -0400

    Links and routes set up. Time to code.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { Switch, Link, Route } from 'react-router-dom'
+import PizzaForm from './components/PizzaForm'
+import Home from './components/Home.js'
+
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
+      <Link to='/'>Home</Link>
+      <Link to ='/pizza'>Pizza?</Link>
+
+      <Switch>
+        <Route path='/pizza/'>
+          <PizzaForm />
+        </Route>
+
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
     </>
   );
 };
+
 export default App;
